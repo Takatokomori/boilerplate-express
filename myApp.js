7f560b2a1641deb208f640d7667f67c64104fa65
editor-taka <takachan1050150@gmail.com>
2023-05-02 13:22:50 +0900

    get.json

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let app = express();
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/views/index.html");
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", function(req, res) {
+  res.json({ "message": "Hello json" });
 });
 
 app.use("/public", express.static(__dirname + "/public"));
@@ -40,4 +44,4 @@ app.use("/public", express.static(__dirname + "/public"));
 
 
 
- module.exports = app;
+module.exports = app;
