f8a9c6d288816971e6482d789c47a3fdac82614b
editor-taka <takachan1050150@gmail.com>
2023-05-02 15:53:56 +0900

    use .env

--- myApp.js
+++ myApp.js
@@ -1,12 +1,20 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
+let response = process.env.MESSAGE_STYLE;
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", function(req, res) {
-  res.json({ "message": "Hello json" });
+    if(response == "uppercase"){
+      response = response.toUpperCase();
+      res.json({"message" : "Hello json".toUpperCase()});
+    } else {
+      response = response.toLowerCase();
+      res.json({"message" : "Hello json"});
+    }
 });
 
 app.use("/public", express.static(__dirname + "/public"));
