4ad1a9f422dd7e3cda94e3c81f24bbce80d1ba94
editor-taka <takachan1050150@gmail.com>
2023-05-02 12:30:57 +0900

    app.use to mount files

--- myApp.js
+++ myApp.js
@@ -3,8 +3,9 @@ let app = express();
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/views/index.html");
-})
-console.log("Hello World");
+});
+
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
